64e8101ac3b5d90b57d6a38a52c83a54a630daa3
Pedro Vidal <pedrorvidal@gmail.com>
2025-03-14 23:17:17 -0300

    switch

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,24 @@
 
 @section('content')
     <h2 class="display-1 text-center">Hello home</h2>
+    {{-- empty --}}
+    @empty($value)
+        <h2>Não existe</h2>
+    @else
+        <h2>Existe</h2>
+    @endempty
+
+    {{-- isset --}}
+    @isset($values)
+        <h2>Existe a variável</h2>
+    @else
+        <h2>Não existe a variável</h2>
+    @endisset
+
+    {{-- unless --}}
+    @unless ($value != 100)
+        <h2>OK</h2>
+    @else
+        <h2>Não existe</h2>
+    @endunless
 @endsection
