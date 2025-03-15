47db1e13b6187a158c72f1f24b35ef35bf7589c6
Pedro Vidal <pedrorvidal@gmail.com>
2025-03-14 22:32:25 -0300

    Views and parameters from controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,6 +10,25 @@ class MainController extends Controller
     //
     public function showView(): View
     {
-        return view('admin.newPage3');
+        //método 1
+        // $data = [
+        //     'name' => 'John Doe',
+        //     'phone' => 393939393,
+        // ];
+        // return view('admin.newPage3', $data);
+
+        //método 2
+        // return view('admin.newPage3', [
+        //     'name' => 'John Doe',
+        //     'phone' => 393939393,
+        // ]);
+
+        //método 3
+        // return view('admin.newPage3')->with('name', 'Pedro Vidal')->with('phone', 222222);
+
+        //método 4 - pesquisa se existem as variáveis do compact em outras parte do código, pra pegar seu valor
+        $name = 'Pedro Vidal';
+        $phone = 222222;
+        return view('admin.newPage3', compact('name', 'phone'));
     }
 }
--- resources/views/admin/newPage3.blade.php
+++ resources/views/admin/newPage3.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
     <h2 class="display-1 text-center">Admin Page</h2>
+    <p>Name: {{ $name }}</p>
+    <p>Phone: {{ $phone }}</p>
 @endsection
