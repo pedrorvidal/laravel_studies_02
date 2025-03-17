321fb4fa8a3b7d9525af0a8fd2888d6c2bbb73a9
Pedro Vidal <pedrorvidal@gmail.com>
2025-03-16 22:29:09 -0300

    csrf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,25 @@
 @extends('layouts.main_layout')
 
 @section('content')
-    @for ($i = 0; $i < 10; $i++)
-        {{-- continue --}}
-        @if ($i == 5)
-            @continue
-        @endif
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <form action="" method="POST">
+                    <div class="mb-3">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" name="username">
+                    </div>
 
-        <p>{{ $i }}</p>
+                    <div class="mb-3">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
 
-        {{-- break --}}
-        @if ($i == 7)
-            @break
-        @endif
-    @endfor
-
-    {{-- loop variable --}}
-    @foreach ($cities as $city)
-        <h3>{{ $loop->index }}</h3>
-        <h2>{{ $city }}</h2>
-        @if ($loop->first)
-            <p>This is the first iteration.</p>
-        @elseif ($loop->last)
-            <p>This is the last iteration.</p>
-        @endif
-    @endforeach
+                    <div class="mb-3">
+                        <button class="btn btn-primary">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
